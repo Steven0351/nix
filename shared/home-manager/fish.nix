fa861ee17a5104b3246198e96e1e3b97a6f22fcf
Steven Sherry <steven.sherry@affinityforapps.com>
2021-12-16 23:01:18 -0600

    Update fish configuration to remove unneeded direnv sourcing

--- shared/home-manager/fish.nix
+++ shared/home-manager/fish.nix
@@ -2,12 +2,12 @@
   programs.fish = {
     enable = true;
     interactiveShellInit = ''
-    cat /Users/stevensherry/.nix-profile/etc/profile.d/nix.sh | /Users/stevensherry/.nix-profile/bin/babelfish | source
-    direnv hook fish | source
+      cat /Users/stevensherry/.nix-profile/etc/profile.d/nix.sh | /Users/stevensherry/.nix-profile/bin/babelfish | source
     '';
     shellAliases = {
       ls = "exa -G --color auto --icons -a -s type";
       ll = "exa -l --color always --icons -a -s type";
+      cat = "bat";
     };
   };
 }
