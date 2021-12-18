1f1946c77e38e87a6c0f6c5b33aa6cee85ef54c0
Steven Sherry <steven.sherry@affinityforapps.com>
2021-12-18 01:44:03 -0600

    Fix indentation in all the lua files touched by stylua before adding stylua.toml

--- shared/nvim/lua/user/alpha.lua
+++ shared/nvim/lua/user/alpha.lua
@@ -1,65 +1,65 @@
 local status_ok, alpha = pcall(require, "alpha")
 if not status_ok then
-	return
+  return
 end
 
 local dashboard = require("alpha.themes.dashboard")
 
 dashboard.section.header.val = {
-   "            :h-                                  Nhy`               ",
-   "           -mh.                           h.    `Ndho               ",
-   "           hmh+                          oNm.   oNdhh               ",
-   "          `Nmhd`                        /NNmd  /NNhhd               ",
-   "          -NNhhy                      `hMNmmm`+NNdhhh               ",
-   "          .NNmhhs              ```....`..-:/./mNdhhh+               ",
-   "           mNNdhhh-     `.-::///+++////++//:--.`-/sd`               ",
-   "           oNNNdhhdo..://++//++++++/+++//++///++/-.`                ",
-   "      y.   `mNNNmhhhdy+/++++//+/////++//+++///++////-` `/oos:       ",
-   " .    Nmy:  :NNNNmhhhhdy+/++/+++///:.....--:////+++///:.`:s+        ",
-   " h-   dNmNmy oNNNNNdhhhhy:/+/+++/-         ---:/+++//++//.`         ",
-   " hd+` -NNNy`./dNNNNNhhhh+-://///    -+oo:`  ::-:+////++///:`        ",
-   " /Nmhs+oss-:++/dNNNmhho:--::///    /mmmmmo  ../-///++///////.       ",
-   "  oNNdhhhhhhhs//osso/:---:::///    /yyyyso  ..o+-//////////:/.      ",
-   "   /mNNNmdhhhh/://+///::://////     -:::- ..+sy+:////////::/:/.     ",
-   "     /hNNNdhhs--:/+++////++/////.      ..-/yhhs-/////////::/::/`    ",
-   "       .ooo+/-::::/+///////++++//-/ossyyhhhhs/:///////:::/::::/:    ",
-   "       -///:::::::////++///+++/////:/+ooo+/::///////.::://::---+`   ",
-   "       /////+//++++/////+////-..//////////::-:::--`.:///:---:::/:   ",
-   "       //+++//++++++////+++///::--                 .::::-------::   ",
-   "       :/++++///////////++++//////.                -:/:----::../-   ",
-   "       -/++++//++///+//////////////               .::::---:::-.+`   ",
-   "       `////////////////////////////:.            --::-----...-/    ",
-   "        -///://////////////////////::::-..      :-:-:-..-::.`.+`    ",
-   "         :/://///:///::://::://::::::/:::::::-:---::-.-....``/- -   ",
-   "           ::::://::://::::::::::::::----------..-:....`.../- -+oo/ ",
-   "            -/:::-:::::---://:-::-::::----::---.-.......`-/.      ``",
-   "           s-`::--:::------:////----:---.-:::...-.....`./:          ",
-   "          yMNy.`::-.--::..-dmmhhhs-..-.-.......`.....-/:`           ",
-   "         oMNNNh. `-::--...:NNNdhhh/.--.`..``.......:/-              ",
-   "        :dy+:`      .-::-..NNNhhd+``..`...````.-::-`                ",
-   "                        .-:mNdhh:.......--::::-`                    ",
-   "                           yNh/..------..`                          ",
-   "                                                                    ",
-   "                              N E O V I M                           ",
+  "            :h-                                  Nhy`               ",
+  "           -mh.                           h.    `Ndho               ",
+  "           hmh+                          oNm.   oNdhh               ",
+  "          `Nmhd`                        /NNmd  /NNhhd               ",
+  "          -NNhhy                      `hMNmmm`+NNdhhh               ",
+  "          .NNmhhs              ```....`..-:/./mNdhhh+               ",
+  "           mNNdhhh-     `.-::///+++////++//:--.`-/sd`               ",
+  "           oNNNdhhdo..://++//++++++/+++//++///++/-.`                ",
+  "      y.   `mNNNmhhhdy+/++++//+/////++//+++///++////-` `/oos:       ",
+  " .    Nmy:  :NNNNmhhhhdy+/++/+++///:.....--:////+++///:.`:s+        ",
+  " h-   dNmNmy oNNNNNdhhhhy:/+/+++/-         ---:/+++//++//.`         ",
+  " hd+` -NNNy`./dNNNNNhhhh+-://///    -+oo:`  ::-:+////++///:`        ",
+  " /Nmhs+oss-:++/dNNNmhho:--::///    /mmmmmo  ../-///++///////.       ",
+  "  oNNdhhhhhhhs//osso/:---:::///    /yyyyso  ..o+-//////////:/.      ",
+  "   /mNNNmdhhhh/://+///::://////     -:::- ..+sy+:////////::/:/.     ",
+  "     /hNNNdhhs--:/+++////++/////.      ..-/yhhs-/////////::/::/`    ",
+  "       .ooo+/-::::/+///////++++//-/ossyyhhhhs/:///////:::/::::/:    ",
+  "       -///:::::::////++///+++/////:/+ooo+/::///////.::://::---+`   ",
+  "       /////+//++++/////+////-..//////////::-:::--`.:///:---:::/:   ",
+  "       //+++//++++++////+++///::--                 .::::-------::   ",
+  "       :/++++///////////++++//////.                -:/:----::../-   ",
+  "       -/++++//++///+//////////////               .::::---:::-.+`   ",
+  "       `////////////////////////////:.            --::-----...-/    ",
+  "        -///://////////////////////::::-..      :-:-:-..-::.`.+`    ",
+  "         :/://///:///::://::://::::::/:::::::-:---::-.-....``/- -   ",
+  "           ::::://::://::::::::::::::----------..-:....`.../- -+oo/ ",
+  "            -/:::-:::::---://:-::-::::----::---.-.......`-/.      ``",
+  "           s-`::--:::------:////----:---.-:::...-.....`./:          ",
+  "          yMNy.`::-.--::..-dmmhhhs-..-.-.......`.....-/:`           ",
+  "         oMNNNh. `-::--...:NNNdhhh/.--.`..``.......:/-              ",
+  "        :dy+:`      .-::-..NNNhhd+``..`...````.-::-`                ",
+  "                        .-:mNdhh:.......--::::-`                    ",
+  "                           yNh/..------..`                          ",
+  "                                                                    ",
+  "                              N E O V I M                           ",
 }
 
 dashboard.section.buttons.val = {
-	dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
-	dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-	dashboard.button("p", "  Find project", ":Telescope projects <CR>"),
-	dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
-	dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
-	dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.lua <CR>"),
+  dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
+  dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+  dashboard.button("p", "  Find project", ":Telescope projects <CR>"),
+  dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
+  dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
+  dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.lua <CR>"),
 }
 
 local function footer()
--- NOTE: requires the fortune-mod package to work
-	-- local handle = io.popen("fortune")
-	-- local fortune = handle:read("*a")
-	-- handle:close()
-	-- return fortune
+  -- NOTE: requires the fortune-mod package to work
+  -- local handle = io.popen("fortune")
+  -- local fortune = handle:read("*a")
+  -- handle:close()
+  -- return fortune
   -- Want to update this to use https://github.com/fredrikburman/ted-lasso-quotes-cli
-	return "To the everlasting glory of the infantry..."
+  return "To the everlasting glory of the infantry..."
 end
 
 dashboard.section.footer.val = footer()
--- shared/nvim/lua/user/cokeline.lua
+++ shared/nvim/lua/user/cokeline.lua
@@ -3,134 +3,134 @@ local colors = require("onenord/colors")
 local get_hex = require("cokeline/utils").get_hex
 
 local space = {
-	text = " ",
+  text = " ",
 }
 
 local separator = {
-	text = "▊",
-	hl = {
-		fg = function(buffer)
-			if buffer.is_focused then
-				return colors.green
-			else
-				return colors.light_gray
-			end
-		end,
-	},
+  text = "▊",
+  hl = {
+    fg = function(buffer)
+      if buffer.is_focused then
+        return colors.green
+      else
+        return colors.light_gray
+      end
+    end,
+  },
 }
 
 local icon = {
-	text = function(buffer)
-		return buffer.devicon.icon
-	end,
-	hl = {
-		fg = function(buffer)
-			if buffer.is_focused then
-				return buffer.devicon.color
-			else
-				return get_hex("Comment", "fg")
-			end
-		end,
-	},
+  text = function(buffer)
+    return buffer.devicon.icon
+  end,
+  hl = {
+    fg = function(buffer)
+      if buffer.is_focused then
+        return buffer.devicon.color
+      else
+        return get_hex("Comment", "fg")
+      end
+    end,
+  },
 }
 
 local filename = {
-	text = function(buffer)
-		return buffer.filename
-	end,
-	hl = {
-		fg = function(buffer)
-			if buffer.is_focused and buffer.is_modified then
-				return colors.yellow
-			end
+  text = function(buffer)
+    return buffer.filename
+  end,
+  hl = {
+    fg = function(buffer)
+      if buffer.is_focused and buffer.is_modified then
+        return colors.yellow
+      end
 
-			if buffer.diagnostics.errors ~= 0 then
-				return colors.red
-			end
+      if buffer.diagnostics.errors ~= 0 then
+        return colors.red
+      end
 
-			if buffer.diagnostics.warnings ~= 0 then
-				return colors.orange
-			end
+      if buffer.diagnostics.warnings ~= 0 then
+        return colors.orange
+      end
 
-			if buffer.diagnostics.infos ~= 0 then
-				return colors.green
-			end
+      if buffer.diagnostics.infos ~= 0 then
+        return colors.green
+      end
 
-			if buffer.diagnostics.hints ~= 0 then
-				return colors.purple
-			end
+      if buffer.diagnostics.hints ~= 0 then
+        return colors.purple
+      end
 
-			if buffer.is_focused then
-				return colors.fg
-			end
-		end,
+      if buffer.is_focused then
+        return colors.fg
+      end
+    end,
 
-		style = function(buffer)
-			if buffer.is_focused then
-				return "bold"
-			end
-		end,
-	},
+    style = function(buffer)
+      if buffer.is_focused then
+        return "bold"
+      end
+    end,
+  },
 }
 
 local changes = {
-	text = function(buffer)
-		if buffer.is_modified then
-			return " ●"
-		else
-			return ""
-		end
-	end,
-	hl = {
-		fg = function(buffer)
-			if buffer.is_focused and buffer.is_modified then
-				return colors.yellow
-			end
-		end,
-	},
+  text = function(buffer)
+    if buffer.is_modified then
+      return " ●"
+    else
+      return ""
+    end
+  end,
+  hl = {
+    fg = function(buffer)
+      if buffer.is_focused and buffer.is_modified then
+        return colors.yellow
+      end
+    end,
+  },
 }
 
 local readonly = {
-	text = function(buffer)
-		if buffer.is_readonly then
-			return " "
-		else
-			return ""
-		end
-	end,
-	hl = {
-		fg = function(buffer)
-			if buffer.is_readonly then
-				return colors.red
-			end
-		end,
-	},
+  text = function(buffer)
+    if buffer.is_readonly then
+      return " "
+    else
+      return ""
+    end
+  end,
+  hl = {
+    fg = function(buffer)
+      if buffer.is_readonly then
+        return colors.red
+      end
+    end,
+  },
 }
 
 cokeline.setup({
-	mappings = {
-		cycle_prev_next = true,
-	},
-	default_hl = {
-		focused = {
-			fg = get_hex("Normal", "fg"),
-			bg = get_hex("ColorColumn", "bg"),
-			style = "bold",
-		},
-		unfocused = {
-			fg = get_hex("Comment", "fg"),
-			bg = get_hex("ColorColumn", "bg"),
-		},
-	},
-	components = {
-		separator,
-		space,
-		space,
-		icon,
-		filename,
-		changes,
-		readonly,
-		space,
-		space,
-	},
+  mappings = {
+    cycle_prev_next = true,
+  },
+  default_hl = {
+    focused = {
+      fg = get_hex("Normal", "fg"),
+      bg = get_hex("ColorColumn", "bg"),
+      style = "bold",
+    },
+    unfocused = {
+      fg = get_hex("Comment", "fg"),
+      bg = get_hex("ColorColumn", "bg"),
+    },
+  },
+  components = {
+    separator,
+    space,
+    space,
+    icon,
+    filename,
+    changes,
+    readonly,
+    space,
+    space,
+  },
 })
