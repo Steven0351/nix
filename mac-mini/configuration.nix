537f93bf4600f786f7618d3b91213f28f6dada6b
Steven Sherry <steven.sherry@affinityforapps.com>
2021-12-22 15:32:45 -0600

    Fix mac-mini configuration.nix

--- mac-mini/configuration.nix
+++ mac-mini/configuration.nix
@@ -1,39 +1,37 @@
-{ config, pkgs, inputs, ...}: {
-    environment.systemPackages = [];
+{ config, pkgs, inputs, ... }: {
+  environment.systemPackages = [ ];
 
-    services.nix-daemon.enable = true;
+  services.nix-daemon.enable = true;
 
-    programs.bash.enable = true;
-    programs.fish.enable = true;
+  programs.bash.enable = true;
+  programs.fish.enable = true;
 
-    environment.pathsToLink = ["/share/fish"];
+  environment.pathsToLink = [ "/share/fish" ];
 
-    system.stateVersion = 4;
+  system.stateVersion = 4;
 
-    nix = {
-      package = pkgs.nix_2_4;
-      extraOptions = ''
-        experimental-features = nix-command flakes
-      '';
-    };
+  nix = {
+    package = pkgs.nix_2_4;
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
 
     gc = {
       automatic = true;
-      interval = {
-        Weekday = 7;
-      };
+      interval = { Weekday = 7; };
       options = "--delete-older-than 7d";
     };
+  };
 
-    users.users = {
-      stevensherry.home = "/Users/stevensherry";
-      build.home = "/Users/build";
-    };
+  users.users = {
+    stevensherry.home = "/Users/stevensherry";
+    build.home = "/Users/build";
+  };
 
-    home-manager.useGlobalPkgs = true;
-    home-manager.users.stevensherry = import ./home.nix;
-    
-    nixpkgs.config.allowUnfree = true;
+  home-manager.useGlobalPkgs = true;
+  home-manager.users.stevensherry = import ./home.nix;
 
-    networking.hostName = "macmini.dev";
-  } 
+  nixpkgs.config.allowUnfree = true;
+
+  networking.hostName = "macmini.dev";
+}
