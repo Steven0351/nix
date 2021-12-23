62bd4350a98c05605b5695194dab927dcd7e7cf2
Steven Sherry <steven.sherry@affinityforapps.com>
2021-12-22 19:00:15 -0600

    Replace manual sourcing with babelfish with nix-env.fish plugin

--- shared/home-manager/fish.nix
+++ shared/home-manager/fish.nix
@@ -1,13 +1,23 @@
-{ pkgs, ...}: {
+{ pkgs, ... }: {
   programs.fish = {
     enable = true;
-    interactiveShellInit = ''
-      cat /Users/stevensherry/.nix-profile/etc/profile.d/nix.sh | /Users/stevensherry/.nix-profile/bin/babelfish | source
-    '';
+    # interactiveShellInit = ''
+    #   cat /Users/stevensherry/.nix-profile/etc/profile.d/nix.sh | /Users/stevensherry/.nix-profile/bin/babelfish | source
+    # '';
     shellAliases = {
       ls = "exa -G --color auto --icons -a -s type";
       ll = "exa -l --color always --icons -a -s type";
       cat = "bat";
     };
+
+    plugins = [{
+      name = "nix-env";
+      src = pkgs.fetchFromGitHub {
+        owner = "lilyball";
+        repo = "nix-env.fish";
+        rev = "7b65bd228429e852c8fdfa07601159130a818cfa";
+        sha256 = "RG/0rfhgq6aEKNZ0XwIqOaZ6K5S4+/Y5EEMnIdtfPhk=";
+      };
+    }];
   };
 }
